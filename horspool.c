97da1512510a17d92f02c56125ddaaf4764bd813
Pranav Bedre <bedrepranav@gmail.com>
2020-03-24 12:34:18 +0530

    Update horspool.c

--- horspool.c
+++ horspool.c
@@ -1,58 +1,86 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-// #include<conio.h>
-#define MAX 500
-int t[MAX],count=0;
-void shifttable(char p[])
+#include <stdio.h>
+#include <string.h>
+
+const int MAX = 256;
+
+int fnHorspool(char string[], char pattern[],int []);
+void fnGenShiftTable(char *,int []);
+
+int main(void)
 {
-    int i,j,m;
-    m=strlen(p);
-    for(i=0;i<MAX;i++)
-    t[i]=m;
-    for(j=0;j<m-1;j++)
-    t[p[j]]=m-1-j;
+	char text[MAX];
+	char pattern[MAX];
+    	int shiftTable[MAX];
+	int found;
+	puts("Enter the source string : ");
+	gets(text);
+	puts("Enter the pattern string : ");
+	gets(pattern);
+	
+	fnGenShiftTable(pattern,shiftTable);
+	found = fnHorspool(text,pattern,shiftTable);
+
+	if(found==-1)
+		puts("\nMatching Substring not found.\n");
+	else
+		printf("\nMatching Substring found at position: %d\n",found+1);
+
+	return 0;
 }
-int horspool(char src[],char p[])
+
+/******************************************************************************
+*Function	: fnGenShiftTable
+*Description	: Function to generate the shift table
+*Input parameters:
+*	char p[] - pattern to be searched for
+*	int t[]	- shift table containing shift values for each alphabet
+*RETURNS	: no value
+******************************************************************************/
+void fnGenShiftTable(char p[], int t[])
 {
-    int i,j,k,m,n;
-    n=strlen(src);
-    m=strlen(p);
-    printf("\nLength of text=%d",n);
-    printf("\n Length of pattern=%d",m);
-    i=m-1;
-    while(i<n)
-    {
-        k=0;
-        while((k<m)&&(p[m-1-k]==src[i-k])){
-            count++;
-            k++;
-        }
-        if(k==m)
-            return(i-m+1);
-        else
-            i+=t[src[i]];
-        count++;
-    }
-    return -1;
+	int m, i, j;
+
+	m = strlen(p);
+
+	for(i=0; i<MAX; i++)
+	{
+		t[i]=m;
+	}
+
+	for(j=0; j<m-1; j++)
+	{
+		t[p[j]] = m-1-j;
+	}
 }
-void main()
+
+/******************************************************************************
+*Function	: fnHorspool
+*Description	: Function to search for a matching substring for a given 
+			pattern in the text
+*Input parameters:
+*	char s[] - text string
+*	char p[] - pattern to be searched for
+*	int t[]	- shift table containing shift values for each alphabet
+*RETURNS	: no value
+******************************************************************************/
+int fnHorspool(char s[],char p[],int t[])
 {
-    char src[100],p[100];
-    int pos;
-    // clrscr();
-    printf("Enter the text in which pattern is to be searched:\n");
-    // gets(src);
-    scanf("%s",src);
-    printf("Enter the pattern to be searched:\n");
-    // gets(p);
-    scanf("%s",p);
-    shifttable(p);
-    pos=horspool(src,p);
-    if(pos>=0)
-        printf("\n The desired pattern was found starting from position %d",pos+1);
-    else
-        printf("\n The pattern was not found in the given text\n");
-    printf("Basic operation count:%d\n",count);
-    // getch();
+	int i, n, m, k;
+
+	n = strlen(s);
+	m = strlen(p);
+	i = m-1;
+	while(i<n)
+	{
+		k = 0;
+		while((k<m)&&(p[m-1-k]==s[i-k]))
+			k++;
+
+		if (k == m)
+			return i-m+1;
+		else
+			i = i+t[s[i]];
+	}
+
+	return -1;
 }
